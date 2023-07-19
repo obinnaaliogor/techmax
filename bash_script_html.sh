0ea0a4139b7d2a06d022bf9456edcebbe29385dd
aliogorobinna <aliogor7509@yahoo.com>
2023-07-20 01:29:36 +0200

    command

--- bash_script_html.sh
+++ bash_script_html.sh
@@ -5,6 +5,9 @@ sudo yum install -y httpd
 sudo yum install -y git
 cd /var/www/html
 git clone https://github.com/obinnaaliogor/techmax.git
+rm -rf techmax/how\ to\ ssh\ into\ instance\ in\ the\ private\ subnet\ with\ powershell.txt
+rm -rf techmax/Command\ to\ ssh\ into\ an\ instance\ in\ the\ private\ subnet.txt
+rm -rf techmax/README.md
 cp -r techmax/* /var/www/html/
 rm -rf techmax
 sudo systemctl enable httpd 
